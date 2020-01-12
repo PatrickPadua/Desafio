25cf4f4240e658f867a4600dd92eac9fceee62ce
Patrick Padua <patrick.padua@cmtecnologia.com.br>
2020-01-12 18:23:22 -0300

    Desafio da Maxmilhas

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+    default = "us-east-1"
+}
+
+variable "aws_sqs_name" {
+    default = "exemplo-sqs-terraform-patrick"
+}
+
+variable "aws_sqs_group" {
+    default = "exemplo-terraform"
+}
